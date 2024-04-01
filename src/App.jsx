7e2ce25487da30f8884387f76fc9363251c0a52d
SamirKhan21860 <msamirkhan561@gmail.com>
2024-04-01 16:13:12 +0500

    Erorr has been solved which was because of wrong placed the Login as a child of Route instead of param Component={Login}

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './Components/Login'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Components/Login";
+import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-      <h2>Disney+ Clone</h2>
       <Router>
-        <Routes exact path="/">
-          <Route>
-            <Login />
-          </Route>
+        <Routes>
+          <Route exact path="/" Component={Login} />
         </Routes>
       </Router>
     </>
   );
 }
 
-export default App
+export default App;
