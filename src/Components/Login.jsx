0201eb84e0e9d91b0ffe234ba0941f551b266559
SamirKhan21860 <msamirkhan561@gmail.com>
2024-04-09 21:21:15 +0500

    images and video with login template is done, working on styling now

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -6,17 +6,20 @@ const Login = () => {
       <section className="container">
         <div className="content">
           <div className="cta">
-            <img className="ctaLogoOne" src="" alt="" />
+            <img className="ctaLogoOne" src="../../" alt="" />
             <a href="" className="singUp"></a>
-
-            <div className="gbImage">
-
-            </div>
+            <p className="description"></p>
+            <img src="" alt="" className="ctalogotwo" />
+          </div>
+          <div className="gbImage">
+            Get Premier Access to Raya and the Last Dragon for an additional fee
+            with a Disney+ subscription. As of 03/26/21, the price of Disney+
+            and The Disney Bundle will increase by $1.
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
 
 // const Container = styled.section`
