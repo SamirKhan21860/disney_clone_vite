dd4839cf2e8ea60e0624e383dd7d22653a8cac89
SamirKhan21860 <msamirkhan561@gmail.com>
2024-04-09 22:04:46 +0500

    adding css to the Login.jsx through external Login.css file by importing it into the compoent

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,33 @@
+import "Login.css";
+
+const Login = () => {
+  return (
+    <>
+      <section className="container">
+        <div className="content">
+          <div className="cta">
+            <img
+              className="ctaLogoOne"
+              src="/images/cta-logo-one.svg"
+              alt="Logo One"
+            />
+            <a href="/signup" className="singUp"></a>
+            <p className="description">
+              Get Premier Access to Raya and the Last Dragon for an additional
+              fee with a Disney+ subscription. As of 03/19/24, the price of
+              Disney+ and The Disney Bundle will increase by $1.
+            </p>
+            <img
+              src="/images/cta-logo-two.png"
+              alt="Log Two"
+              className="ctalogotwo"
+            />
+          </div>
+          <div className="gbImage"></div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
