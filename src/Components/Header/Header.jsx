bb5b892c9aba7fc642dfea5b6e071239f2e4b838
SamirKhan21860 <msamirkhan561@gmail.com>
2024-04-16 20:41:27 +0500

    Firebase mestry solved

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,74 +1,43 @@
-import { auth, provider } from "../../firebase";
+import { auth, googleProvider } from "../../firebase";
+import { signInWithPopup, signOut } from "firebase/auth";
 import "./Header.css";
 import { useDispatch, useSelector } from "react-redux";
-// import { useHistory } from "react-router-dom"; // useHisotry is no more in React V6+
 import { useNavigate } from "react-router-dom";
 import {
   selectUserName,
   setSignOutState,
-  selectUserPhoto,
   setUserLoginDetails,
 } from "../../features/user/userSlice";
-import { useCallback, useEffect } from "react";
 
 const Header = () => {
+
+  // 
   const dispatch = useDispatch();
   const history = useNavigate();
   const userName = useSelector(selectUserName);
-  const userPhoto = useSelector(selectUserPhoto);
 
-  const setUser = useCallback(
-    (user) => {
-      // Your logic for setting user details in Redux store
-      dispatch(
-        setUserLoginDetails({
-          name: user.displayName,
-          email: user.email,
-          photo: user.photoURL,
-        })
-      );
-    },
-    [dispatch]
-  ); // Dependencies for useCallback
-
-  useEffect(() => {
-    auth.onAuthStateChanged(async (user) => {
-      if (user) {
-        setUser(user);
-        // history.push("/home");
-        history("/home");
-      }
-    });
-  }, [history, setUser]);
-
-  useEffect(() => {
-    // This effect runs whenever userName changes
-    if (userName) {
-      // Update UI based on logged-in user's name
-      console.log(`Logged in user: ${userName}`); // Example update
-    }
-  }, [userName]); // Dependency array with userName
-
-  const handleAuth = () => {
-    if (!userName) {
-      auth
-        .signInWithPopup(provider)
-        .then((result) => {
-          setUser(result.user);
-        })
-        .catch((error) => {
-          alert(error.message);
-        });
-    } else if (userName) {
-      auth
-        .signOut()
-        .then(() => {
-          dispatch(setSignOutState());
-          history.push("/");
-        })
-        .catch((err) => alert(err.message));
+  const handleAuth = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      console.log(result);
+      const user = result.user;
+      dispatch(setUserLoginDetails(user));
+      history("/home");
+      await signInWithPopup(auth, googleProvider)
+    } catch (err) {
+      console.log(err);
     }
   };
+
+  const logOut = async () => {
+    try {
+      await signOut(auth);
+      dispatch(setSignOutState());
+      history("/");
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
     <>
@@ -110,9 +79,13 @@ const Header = () => {
               </a>
             </div>
             <div className="sign-out">
-              <img className="user-img" src={userPhoto} alt={userName} />
+              <img
+                className="user-img"
+                src={userName.photoURL}
+                alt={userName.displayName}
+              />
               <div className="drop-down">
-                <span onClick={handleAuth}>Sign out</span>
+                <span onClick={logOut}>Sign out</span>
               </div>
             </div>
           </>
@@ -123,3 +96,130 @@ const Header = () => {
 };
 
 export default Header;
+
+// import { auth, provider } from "../../firebase";
+// import "./Header.css";
+// import { useDispatch, useSelector } from "react-redux";
+// // import { useHistory } from "react-router-dom"; // useHisotry is no more in React V6+
+// import { useNavigate } from "react-router-dom";
+// import {
+//   selectUserName,
+//   setSignOutState,
+//   selectUserPhoto,
+//   setUserLoginDetails,
+// } from "../../features/user/userSlice";
+// import { useCallback, useEffect } from "react";
+// import { signInWithPopup } from "firebase/auth";
+
+// const Header = () => {
+//   const dispatch = useDispatch();
+//   const history = useNavigate();
+//   const userName = useSelector(selectUserName);
+//   const userPhoto = useSelector(selectUserPhoto);
+
+//   const setUser = useCallback(
+//     (user) => {
+//       // Your logic for setting user details in Redux store
+//       dispatch(
+//         setUserLoginDetails({
+//           name: user.displayName,
+//           email: user.email,
+//           photo: user.photoURL,
+//         })
+//       );
+//     },
+//     [dispatch]
+//   ); // Dependencies for useCallback
+
+//   useEffect(() => {
+//     auth.onAuthStateChanged(async (user) => {
+//       if (user) {
+//         setUser(user);
+//         // history.push("/home");
+//         history("/home");
+//       }
+//     });
+//   }, [history, setUser]);
+
+//   useEffect(() => {
+//     // This effect runs whenever userName changes
+//     if (userName) {
+//       // Update UI based on logged-in user's name
+//       console.log(`Logged in user: ${userName}`); // Example update
+//     }
+//   }, [userName]); // Dependency array with userName
+
+//   const handleAuth = () => {
+//     if (!userName) {
+//       auth
+//         .signInWithPopup(provider)
+//         .then((result) => {
+//           setUser(result.user);
+//         })
+//         .catch((error) => {
+//           alert(error.message);
+//         });
+//     } else if (userName) {
+//       auth
+//         .signOut()
+//         .then(() => {
+//           dispatch(setSignOutState());
+//           history.push("/");
+//         })
+//         .catch((err) => alert(err.message));
+//     }
+//   };
+
+//   return (
+//     <>
+//       <nav className="nav">
+//         <a className="logo">
+//           <img src="/images/logo.svg" alt="Disney+" />
+//         </a>
+
+//         {!userName ? (
+//           <div className="login">
+//             <a onClick={handleAuth}>Login</a>
+//           </div>
+//         ) : (
+//           <>
+//             <div className="nav-menu">
+//               <a href="/home">
+//                 <img src="/images/home-icon.svg" alt="HOME" />
+//                 <span>HOME</span>
+//               </a>
+//               <a>
+//                 <img src="/images/search-icon.svg" alt="SEARCH" />
+//                 <span>SEARCH</span>
+//               </a>
+//               <a>
+//                 <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+//                 <span>WATCHLIST</span>
+//               </a>
+//               <a>
+//                 <img src="/images/original-icon.svg" alt="ORIGINALS" />
+//                 <span>ORIGINALS</span>
+//               </a>
+//               <a>
+//                 <img src="/images/movie-icon.svg" alt="MOVIES" />
+//                 <span>MOVIES</span>
+//               </a>
+//               <a>
+//                 <img src="/images/series-icon.svg" alt="SERIES" />
+//                 <span>SERIES</span>
+//               </a>
+//             </div>
+//             <div className="sign-out">
+//               <img className="user-img" src={userPhoto} alt={userName} />
+//               <div className="drop-down">
+//                 <span onClick={handleAuth}>Sign out</span>
+//               </div>
+//             </div>
+//           </>
+//         )}
+//       </nav>
+//     </>
+//   );
+// };
+
+// export default Header;
