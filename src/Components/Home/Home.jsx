1e9ed3c8aa17f7a3df67b0e2d6ec7c5f756d4d85
SamirKhan21860 <msamirkhan561@gmail.com>
2024-04-19 13:02:37 +0500

    back with styled-components

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,22 +6,20 @@ import Recommends from "../Recommends/Recommends";
 import Trending from "../Trending/Trending";
 import Viewers from "../Viewers/Viewers";
 import "./Home.css";
-import { useDispatch, useSelector } from "react-redux";
-import { selectUserName } from "../../features/user/userSlice";
+import { useDispatch } from "react-redux";
 import { setMovies } from "../../features/movie/movieSlice";
+import { collection, onSnapshot } from "firebase/firestore";
 import db from "../../firebase";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const userName = useSelector(selectUserName);
   const recommendsRef = useRef([]);
   const newDisneyRef = useRef([]);
   const originalsRef = useRef([]);
   const trendingRef = useRef([]);
 
   useEffect(() => {
-    console.log("hello");
-    db.collection("movies").onSnapshot((snapshot) => {
+    const unsubscribe = onSnapshot(collection(db, "movies"), (snapshot) => {
       snapshot.docs.forEach((doc) => {
         const movieData = { id: doc.id, ...doc.data() };
         switch (movieData.type) {
@@ -51,20 +49,93 @@ const Home = () => {
         })
       );
     });
-  }, [userName, dispatch]);
+
+    // Cleanup function to unsubscribe from snapshot updates when the component unmounts
+    return () => unsubscribe();
+  }, [dispatch]);
 
   return (
-    <>
-      <div className="container">
-        <ImgSlider />
-        <Viewers />
-        <Recommends />
-        <NewDisney />
-        <Originals />
-        <Trending />
-      </div>
-    </>
+    <div className="container">
+      <ImgSlider />
+      <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
+    </div>
   );
 };
 
 export default Home;
+
+// import Originals from "../Originals/Originals";
+// import ImgSlider from "../ImgSlider/ImgSlider";
+// import NewDisney from "../NewDisney/NewDisney";
+// import Recommends from "../Recommends/Recommends";
+// import Trending from "../Trending/Trending";
+// import Viewers from "../Viewers/Viewers";
+// import "./Home.css";
+// import { useDispatch, useSelector } from "react-redux";
+// import { selectUserName } from "../../features/user/userSlice";
+// import { setMovies } from "../../features/movie/movieSlice";
+// import db from "../../firebase";
+// import { useEffect } from "react";
+
+// const Home = () => {
+//   const dispatch = useDispatch();
+//   const userName = useSelector(selectUserName);
+//   let recommends = [];
+//   let newDisneys = [];
+//   let originals = [];
+//   let trending = [];
+
+//   useEffect(() => {
+//     console.log("hello");
+//     db.collection("movies").onSnapshot((snapshot) => {
+//       snapshot.docs.map((doc) => {
+//         console.log(recommends);
+//         switch (doc.data().type) {
+//           case "recommend":
+//             recommends = [...recommends, { id: doc.id, ...doc.data() }];
+//             break;
+
+//           case "new":
+//             newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
+//             break;
+
+//           case "original":
+//             originals = [...originals, { id: doc.id, ...doc.data() }];
+//             break;
+
+//           case "trending":
+//             trending = [...trending, { id: doc.id, ...doc.data() }];
+//             break;
+//         }
+//       });
+
+//       dispatch(
+//         setMovies({
+//           recommend: recommends,
+//           newDisney: newDisneys,
+//           original: originals,
+//           trending: trending,
+//         })
+//       );
+//     });
+//   }, [userName]);
+
+//   return (
+//     <>
+//       <div className="container">
+//         <ImgSlider />
+//         <Viewers />
+//         <Recommends />
+//         <NewDisney />
+//         <Originals />
+//         <Trending />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Home;
--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,5 +1,4 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// import firebase from "firebase";
 
 // const firebaseConfig = {
 //   apiKey: "AIzaSyCupowt0dwOO79qSr3dWfScd1ZmwzmSpeQ",
@@ -11,41 +10,65 @@
 //   measurementId: "G-6RBQED6T5H",
 // };
 
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+// const provider = new firebase.auth.GoogleAuthProvider();
+// const storage = firebase.storage();
 
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-// export const googleProvider = new GoogleAuthProvider();
-
-
+// export { auth, provider, storage };
+// export default db;
 
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCupowt0dwOO79qSr3dWfScd1ZmwzmSpeQ",
-
   authDomain: "disneyplus-7d1d0.firebaseapp.com",
-
   projectId: "disneyplus-7d1d0",
-
   storageBucket: "disneyplus-7d1d0.appspot.com",
-
   messagingSenderId: "968286701383",
-
   appId: "1:968286701383:web:9ecdbdc52cda6d91caee62",
-
   measurementId: "G-6RBQED6T5H",
 };
 
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
 
-// Export Firebase services
-export const auth = getAuth(firebaseApp);
-export const provider = new GoogleAuthProvider();
-export const storage = getStorage(firebaseApp);
-export const db = getFirestore(firebaseApp);
-
+export { auth, provider };
 export default db;
+
+// import { initializeApp } from "firebase/app";
+// import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// import { getStorage } from "firebase/storage";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCupowt0dwOO79qSr3dWfScd1ZmwzmSpeQ",
+
+//   authDomain: "disneyplus-7d1d0.firebaseapp.com",
+
+//   projectId: "disneyplus-7d1d0",
+
+//   storageBucket: "disneyplus-7d1d0.appspot.com",
+
+//   messagingSenderId: "968286701383",
+
+//   appId: "1:968286701383:web:9ecdbdc52cda6d91caee62",
+
+//   measurementId: "G-6RBQED6T5H",
+// };
+
+// // Initialize Firebase
+// const firebaseApp = initializeApp(firebaseConfig);
+
+// // Export Firebase services
+// export const auth = getAuth(firebaseApp);
+// export const provider = new GoogleAuthProvider();
+// export const storage = getStorage(firebaseApp);
+// export const db = getFirestore(firebaseApp);
+
+// export default db;
